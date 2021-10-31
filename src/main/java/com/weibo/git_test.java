4ca5b6017256f73d168203e57dc82f35e81ece0c
zhangyong8 <zhangyong8@staff.weibo.com>
2021-10-31 14:37:51 +0800

    first commit

---
+++ src/main/java/com/weibo/git_test.java
@@ -0,0 +1,12 @@
+package com.weibo;
+
+/**
+ * @Auther: zhangyong
+ * @Date: 2021/10/31 - 10 -31
+ * @Description: com.weibo
+ */
+public class git_test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
