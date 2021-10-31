c186e2980051eea03ff18b0196fcb8dcbe9b525a
zhangyong8 <zhangyong8@staff.weibo.com>
2021-10-31 14:45:56 +0800

    second commit

--- src/main/java/com/weibo/git_test.java
+++ src/main/java/com/weibo/git_test.java
@@ -8,5 +8,6 @@ package com.weibo;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("second change");
     }
 }
