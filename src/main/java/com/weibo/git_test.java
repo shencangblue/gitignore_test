2db3fe50cdd1e06bcf336e5beb216e2bf3bb2022
shencangblue <46117819+shencangblue@users.noreply.github.com>
2021-10-31 18:40:10 +0800

    pull commit

--- src/main/java/com/weibo/git_test.java
+++ src/main/java/com/weibo/git_test.java
@@ -14,5 +14,6 @@ public class git_test {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push gitignore_test");
+        System.out.println("pull gitignore_test");
     }
 }
